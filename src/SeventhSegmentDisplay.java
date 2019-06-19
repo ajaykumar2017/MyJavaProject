ceb46a9b8ff7462c672e18577951d94f56d4f2bd
Ajay Kumar <ajaykumar201761@gmail.com>
2019-06-19 15:39:41 +0530

    Seventh segment display 1 test case passed

--- src/SeventhSegmentDisplay.java
+++ src/SeventhSegmentDisplay.java
@@ -1,5 +1,15 @@
-import java.util.*;
+/*
+*Alice got a number written in seven segment format where each segment
+  was creatted used a matchstick.
 
+Example: If Alice gets a number 123 so basically Alice used
+ 12 matchsticks for this number.
+
+Alice is wondering what is the numerically largest value
+ that she can generate by using at most the matchsticks that she currently possess.Help Alice out by telling her that number.
+ */
+import java.math.BigInteger;
+import java.util.*;
 public class SeventhSegmentDisplay {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
@@ -12,11 +22,44 @@ public class SeventhSegmentDisplay {
         }
         for(int i=0;i<T;i++)
         {
-            System.out.println(maxNumberFromSticks(noSeg[N[i]]));
+            int temp=N[i];
+            int matches=0;
+            if (temp==0){
+                matches=6;
+            }else{
+                while (temp!=0)
+                {
+                    matches=matches+noSeg[temp%10];
+                    temp=temp/10;
+                }
+            }
+
+            System.out.println(maxNumberFromSticks(BigInteger.valueOf(matches)));
+//            System.out.println(matches);
         }
     }
-    static int maxNumberFromSticks(int n)
+    private static BigInteger maxNumberFromSticks(BigInteger n)
     {
-        return n;
+        BigInteger temp = new BigInteger("0");
+        BigInteger num=n;
+        while (n.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))&&
+                num.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))&&
+                n.compareTo(BigInteger.valueOf(0))>0&&
+                num.compareTo(BigInteger.valueOf(0))>0){
+            temp=(temp.multiply(BigInteger.valueOf(10))).add(BigInteger.valueOf(1));
+            num=num.subtract(BigInteger.valueOf(2));
+        }
+        if (!(n.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0)))){
+            num=num.subtract(BigInteger.valueOf(3));
+            temp=BigInteger.valueOf(7);
+            while (!n.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))&&
+                    num.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0))&&
+                    n.compareTo(BigInteger.valueOf(0))>0&&
+                    num.compareTo(BigInteger.valueOf(0))>0){
+                temp=(temp.multiply(BigInteger.valueOf(10))).add(BigInteger.valueOf(1));
+                num=num.subtract(BigInteger.valueOf(2));
+            }
+        }
+        return temp;
     }
 }
